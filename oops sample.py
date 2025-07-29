94db6f369a6f82a91e5307dc08f3c6519384bb73
shahidhassankh <harisshameem71@gmail.com>
2025-07-29 13:50:19 +0530

    oops inheritance

--- oops sample.py
+++ oops sample.py
@@ -8,7 +8,7 @@ Lap1=Laptops()
 Lap1.spec()
 print(Lap1.developer)"""
 
-class Vehicles:
+"""class Vehicles:
     def __init__(self,m,model):
         self.make=m
         self.model=model
@@ -19,5 +19,70 @@ class Car(Vehicles):
         self.power=p
         super().__init__(m,model)
 c1=Car("Red","151 Bhp","2006","Kia Seltos")
-print(c1.make,c1.color)
+print(c1.mak,c1.color)"""
         
+        #mutlple inheritance
+
+"""class father:
+    def skills(self):
+        print("father:carpenter,gardening" )
+
+class mother:
+    def skills(self):
+        print("mother:cooking,caring")
+
+class child(father,mother):
+    def skills (self):
+        father.skills(self)
+        mother.skills(self)
+        print("child:playing football")
+c=child()
+c.skills()"""
+
+           #multilevel inheritance
+#Grandparent (Base class)
+      #↓
+ #  Parent (Derived from Grandparent)
+     # ↓
+  # Child (Derived from Parent)
+
+
+"""class grandfather:
+    def vintageclub(self):
+        print("grandfather:real madrid")
+class father(grandfather):
+    def modernclub(self):
+        print("father:barcelona")
+class child(father):
+    def chappclub(self):
+        print("child:kerala blasters")
+c=child()
+c.vintageclub()
+c.modernclub()
+c.chappclub()"""
+   
+         #Hierarchieal inheritance
+
+     #Parent
+     #/     \
+#Child1     Child2
+
+
+"""class india:
+    def side(self):
+        print("india is divided into two sides")
+class southside(india):
+    def south_looks(self):
+        print("south india: rural and tropical")
+class northside(india):
+    def north_looks(self):
+        print("north india:urban and polluted")
+s=southside()
+s.side()
+s.south_looks()
+
+n=northside()
+n.side()
+n.north_looks()"""
+
+       
