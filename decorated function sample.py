58452bf6754cd508f91d6021ad938a86c646dec3
shahidhassankh <harisshameem71@gmail.com>
2025-07-30 13:39:54 +0530

    decoraters

---
+++ decorated function sample.py
@@ -0,0 +1,19 @@
+"""def decorate(func):
+    def wrapper():
+        print("visca el barca")
+        func()
+        print("we play as a team")
+    return wrapper
+@decorate  
+def greet():
+    print("hello teams")
+
+greet()"""
+
+
+
+"""def swap_decorator(func):
+    def wrapper(a, b):
+        print(f"Before swapping: a = {a}, b = {b}")
+        a, b = b, a  
+        print"""
